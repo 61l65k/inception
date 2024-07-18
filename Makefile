a519d80ee38244589cdc515bccbbd35fc02ef603
alex <alexpyykonen@gmail.com>
2024-07-18 20:22:36 +0300

    + Add basic script for setting up the docker and the desktop enviroment if building from server image >:0 + Add check before cleaning the whole docker on the system to Makefile

--- Makefile
+++ Makefile
@@ -8,7 +8,7 @@ check-volume-directories:
 	@mkdir -p ~/data/mariadb-volume
 	@mkdir -p ~/data/portainer-volume
 
-up: check-directories
+up: check-volume-directories
 	@docker compose -f $(DOCKER_COMPOSE_FILE) up -d --build
 
 down:
@@ -19,14 +19,23 @@ logs:
 
 re: down up
 
-clean:
+clean: confirm-clean
 	@docker stop $$(docker ps -qa) || true
 	@docker rm $$(docker ps -qa) || true
 	@docker rmi -f $$(docker images -qa) || true
 	@docker volume rm $$(docker volume ls -q) || true
 	@docker network rm $$(docker network ls -q) || true
 
+confirm-clean:
+	@read -p "Are you sure you want to clean all Docker data from this host? This action cannot be undone. [y/N]: " confirm && \
+	if [ "$$confirm" = "y" ]; then \
+		echo "Cleaning Docker data..."; \
+	else \
+		echo "Clean operation cancelled."; \
+		exit 1; \
+	fi
+
 clean-local-volumes:
 	@rm -rf ~/data
 
-.PHONY: all up down re clean clean-local-volumes check-directories
+.PHONY: all up down re clean clean-local-volumes check-volume-directories confirm-clean
---
+++ setup_all.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+setup_docker()
+{
+    sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+    sudo add-apt-repository \
+       "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+       $(lsb_release -cs) \
+       stable"
+    sudo apt-get update
+    sudo apt-get install -y docker-ce
+    sudo curl -L "https://github.com/docker/compose/releases/download/v2.13.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo usermod -aG docker $USER
+    newgrp docker
+    docker --version
+    docker compose version
+    echo "Please log out and log back in to apply Docker group membership"
+}
+
+setup_minimal_desktop_gui()
+{
+    sudo apt-get install -y lxde gdm3
+    sudo apt-get install -y xorg
+    sudo apt-get install -y network-manager-gnome
+    sudo dpkg-reconfigure gdm3
+    echo "LXDE and gdm3 installation is complete. You can reboot your system to start the graphical environment."
+}
+
+setup_dns_to_local()
+{
+    sudo echo "127.0.0.1 $USER" >> /etc/hosts
+}
+
+
+main()
+{
+    echo "Run this script as root user press enter to continue"
+    echo "This script will setup the docker for you and install minimal desktop GUI and setup DNS to local"
+    read
+    setup_docker
+    read -p "Do you want to install minimal desktop GUI? [y/n]: " install_gui
+    if [ "$install_gui" == "y" ]; then
+        setup_minimal_desktop_gui
+    fi
+    setup_dns_to_local
+}
