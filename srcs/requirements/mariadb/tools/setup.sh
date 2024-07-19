515580a73ec39ed3cc99240f531e0845aa30c9c5
alex <alexxpyykonen@gmail.com>
2024-07-19 13:45:54 +0300

    Add check to mariadb if the db exists already dont init it

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -6,7 +6,16 @@ mysql_pid=$!
 
 sleep 5
 
-mysql -u root <<EOF
+db_exists() {
+    mysql -u root -p"$DB_ROOT_PASSWORD" -e "USE $DB_NAME;" 2>/dev/null
+}
+
+if mysql -u root -p"$DB_ROOT_PASSWORD" -e ";" 2>/dev/null && db_exists; then
+    echo "Database already exists. Skipping initialization."
+else
+    echo "Setting up MySQL root user, database, and user..."
+
+    mysql -u root <<EOF
 CREATE DATABASE IF NOT EXISTS $DB_NAME;
 CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
 GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
@@ -15,6 +24,7 @@ ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';
 ALTER USER 'root'@'%' IDENTIFIED BY '$DB_ROOT_PASSWORD';
 FLUSH PRIVILEGES;
 EOF
+fi
 
 mysqladmin -u root -p"$DB_ROOT_PASSWORD" shutdown
 
